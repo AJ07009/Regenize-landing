3608c116e0f8c9b520f99ab76b00455e63d69b29
AJ07009 <you@example.com>
2019-07-30 14:16:26 +0200

    aidans

---
+++ Desktop/code/proxy/front-end-project/front-end/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'user-login-page', loadChildren: './user-login-page/user-login-page.module#UserLoginPagePageModule' },
+  { path: 'registration', loadChildren: './registration/registration.module#RegistrationPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
